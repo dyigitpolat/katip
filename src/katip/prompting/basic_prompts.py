e78b7faf5d0cbc0e9b335a939ee223ddbe127974
dyigitpolat <dyigit96@gmail.com>
2023-05-04 22:22:53 +0800

    fix prompt

--- src/katip/prompting/basic_prompts.py
+++ src/katip/prompting/basic_prompts.py
@@ -5,4 +5,4 @@ class BasicPrompts:
     section_body_task_template = """Provide a fully detailed body (or intro) for the {section_name} section"""
     subsection_body_task_template = """Provide a fully detailed body for the {subsection_name} subsection of the {section_name} section"""
     additional_text_instructions = \
-"""Refer to other work and experiments as much as possible. To cite, always use the syntax ((citation_needed: <put meaningful keywords about relevant studies here (WITHOUT any author names)>)). to refer to experiments and results, always use the syntax ((result_needed: <put a short description of the required experiment here>)). Refer to other work and experiments as much as possible using our special ((citation_needed: ...)) or ((result_needed: ...)) syntax."""
+"""Refer to other work and experiments as much as possible. Always use the syntax ((result_needed: <short description of experiment>)) for referring to experiments and results. Always use the syntax ((citation_needed: <keywords and topics about relevant work, without author names>)) for referring to other work. """
