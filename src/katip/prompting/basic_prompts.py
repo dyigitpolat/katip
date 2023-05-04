887274ad390be44429f045e285434f3a36cf403e
dyigitpolat <dyigit96@gmail.com>
2023-05-04 16:59:40 +0800

    tweak prompt

--- src/katip/prompting/basic_prompts.py
+++ src/katip/prompting/basic_prompts.py
@@ -1,6 +1,6 @@
 class BasicPrompts:
     json_introducer = """using the following JSON protocol:\n"""
     prefix_introducer = """\n\nComplete the response as parsable JSON (use proper escape sequences, mind the JSON syntax etc.):\n```json\n"""
-    draft_task = """Using the abstract, provide a draft for this scientific paper"""
+    draft_task = """Using the abstract, provide a draft for this scientific paper, complete with all crucial sections and subsections"""
     section_body_task_template = """Provide a fully detailed body (or intro) for the {section_name} section"""
     subsection_body_task_template = """Provide a fully detailed body for the {subsection_name} subsection of the {section_name} section"""
