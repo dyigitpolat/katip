7308f98fab9061a08bab8fef3720a2e044e87e53
dyigitpolat <dyigit96@gmail.com>
2023-05-04 16:57:59 +0800

    ask user for context

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
+from katip.content_generation.abstract_generator import AbstractGenerator
+from katip.content_generation.elaborator import Elaborator
 from katip.content_generation.document_generator import DocumentGenerator
 from katip.renderers.basic_latex_renderer import BasicLatexRenderer
 
 import json
 import os
 
-abstract = """This paper presents MimarSinan, an end-to-end hardware-aware spiking neural network architecture search (NAS) framework for neuromorphic AI accelerators. MimarSinan aims to optimize the performance and energy efficiency of spiking neural networks (SNNs) by jointly searching for the SNN topology, neuron model, and synaptic model. Unlike existing NAS frameworks, MimarSinan considers hardware-specific constraints and characteristics such as the number of available resources, the dataflow of the accelerator, and the energy consumption of the circuits. To efficiently explore the vast design space, MimarSinan employs a gradient-based optimization algorithm and a surrogate model to approximate the hardware cost of the searched architectures. Experimental results demonstrate that MimarSinan can find high-performance SNN architectures that outperform state-of-the-art designs while meeting the hardware constraints. MimarSinan is a promising approach to enable the design of efficient neuromorphic AI accelerators with improved accuracy and energy consumption."""
-
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     os.makedirs("../generated", exist_ok=True)
 
+    context = input("provide context: _")
+    abstract = AbstractGenerator().generate_abstract(context)
+    abstract = Elaborator().elaborate_abstract(abstract)
+
     document_dict = DocumentGenerator().generate_from_abstract(abstract)
     json.dump(document_dict, open("../generated/document.json", "w"))
 
-    document_dict = json.load(open("../generated/document_v0.json"))
+    document_dict = json.load(open("../generated/document.json"))
     latex_source = BasicLatexRenderer().render(document_dict, "Yigit Polat")
 
     with open("../generated/document.tex", "w") as f:
