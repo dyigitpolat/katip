a9068c096fb11fd33e54bcfa40c5d42ab2dadbee
dyigitpolat <dyigit96@gmail.com>
2023-05-04 16:57:43 +0800

    abstract elaboration

--- src/katip/content_generation/elaborator.py
+++ src/katip/content_generation/elaborator.py
@@ -7,9 +7,19 @@ class Elaborator:
         protocol_path = os.getcwd() + "/katip/protocols/paragraph.json"
         paragraph_protocol = json.load(open(protocol_path))
 
-        context += "\n\n" + json.dumps(paragraph)
+        context += "\n\n" + json.dumps(paragraph, indent=2)
         return ProtocolHandler().get_structured_response(
             context,
             paragraph_protocol,
-            "Add more content and elaborate the paragraph",
+            "Add more content and elaborate the given paragraph (but retain the main ideas)",
             "paragraph_name")
+    
+    def elaborate_abstract(self, abstract):
+        protocol_path = os.getcwd() + "/katip/protocols/abstract.json"
+        abstract_protocol = json.load(open(protocol_path))
+
+        return ProtocolHandler().get_structured_response(
+            abstract,
+            abstract_protocol, 
+            "Add more content and elaborate the abstract",
+            "title")["abstract"]
