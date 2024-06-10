d5e7aa3ad8b1c18464d4c06174eaa76ac025afd7
dyigitpolat <dyigit96@gmail.com>
2024-06-10 20:10:07 +0000

    setup update

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
 openai_api_key = input("Enter your OpenAI API key: _")
-open("src/api_keys.py", "w").write(f"openai_api_key = \"{openai_api_key}\"")
+semanticscholar_api_key = input("Enter your Semantic Scholar API key: _")
+katip_token = input("Enter your Katip token: _")
+
+key_str = f"openai_api_key = \"{openai_api_key}\"\nkatip_token = \"{katip_token}\"\nss_api_key = \"{semanticscholar_api_key}\""
+
+open("src/api_keys.py", "w").write(key_str)
